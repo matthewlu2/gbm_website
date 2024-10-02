c292547ca1b199abf3300a9ccf4f10dc124a66a3
xim2016 <xj_ma@yahoo.com>
2024-10-02 18:08:50 -0400

    Update spatial_gene.py

--- views/spatial_gene.py
+++ views/spatial_gene.py
@@ -7,14 +7,12 @@ IMG_REPO = 'https://raw.githubusercontent.com/matthewlu2/gbm_data/main/spatial_g
 
 
 st.markdown("<h2 style='text-align: center; color: black;'>Spatial gene expression</h1>", unsafe_allow_html=True)  
+st.write("")
 
 file = open('text_files/spatial_gene_names_10.txt', 'r')
 list = file.read().splitlines()
 
-sample_list = ['MGH258', 'UKF243', 'UKF248', 'UKF251', 'UKF255', 'UKF259', 'UKF260',
-                'UKF266', 'UKF269', 'UKF275', 'UKF296', 'UKF304', 'UKF313', 'UKF334',
-                'ZH1007inf', 'ZH1007nec', 'ZH1019T1', 'ZH1019inf', 'ZH8811Abulk', 'ZH8811Bbulk',
-                'ZH8812bulk', 'ZH881T1', 'ZH881inf', 'ZH916T1', 'ZH916bulk', 'ZH916inf']
+
 
 a, b = st.columns(2)
 
