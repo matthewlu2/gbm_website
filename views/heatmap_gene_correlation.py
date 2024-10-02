b244584aa3b6f9811c7051df96ab360fb511b6c4
xim2016 <xj_ma@yahoo.com>
2024-10-02 18:09:52 -0400

    Update heatmap_gene_correlation.py

--- views/heatmap_gene_correlation.py
+++ views/heatmap_gene_correlation.py
@@ -3,7 +3,7 @@ from streamlit_option_menu import option_menu
 from streamlit_pdf_viewer import pdf_viewer
 
 st.markdown("<h2 style='text-align: center; color: black;'>Correlation heatmaps</h1>", unsafe_allow_html=True)  
-
+st.write("")
 
 file2 = open('text_files/correlation_per_gene_names.txt', 'r')
 list2 = file2.read().splitlines()
