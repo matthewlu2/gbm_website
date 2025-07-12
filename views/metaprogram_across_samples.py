2a797aff272c790d47cd9b444fb4d6f4f9e55901
xim2016 <xj_ma@yahoo.com>
2025-07-12 19:29:34 -0400

    Update metaprogram_across_samples.py

--- views/metaprogram_across_samples.py
+++ views/metaprogram_across_samples.py
@@ -7,9 +7,9 @@ st.markdown("<h2 style='text-align: center; color: black;'>Metaprogram-specific<
 st.write("")
 
 st.info("Compare how regulatory features linked to transcriptional metaprograms vary across multiple glioblastoma samples.<br>
-•	TF: Examine transcription factors associated with each metaprogram to see which are consistently or differentially active across tumors.<br>
-•	Pathway: Explore pathway activation linked to specific metaprograms, uncovering shared or distinct signaling programs.<br>
-•	Drug: Assess predicted drug response signatures tied to different metaprograms, highlighting therapeutic vulnerabilities across the cohort.<br>
+  • TF: Examine transcription factors associated with each metaprogram to see which are consistently or differentially active across tumors.<br>
+  • Pathway: Explore pathway activation linked to specific metaprograms, uncovering shared or distinct signaling programs.<br>
+  • Drug: Assess predicted drug response signatures tied to different metaprograms, highlighting therapeutic vulnerabilities across the cohort.<br>
 Use interactive plots and selectors to identify regulatory patterns and drug sensitivities tied to key malignant and non-malignant programs."
 
 
