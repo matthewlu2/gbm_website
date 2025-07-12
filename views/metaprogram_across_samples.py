585ec420a6410fc3501a07f6950fb467b5cb8044
xim2016 <xj_ma@yahoo.com>
2025-07-12 19:36:18 -0400

    Update metaprogram_across_samples.py

--- views/metaprogram_across_samples.py
+++ views/metaprogram_across_samples.py
@@ -6,12 +6,11 @@ IMG_REPO_2 = 'https://raw.githubusercontent.com/matthewlu2/gbm_small_data/main/d
 st.markdown("<h2 style='text-align: center; color: black;'>Metaprogram-specific</h1>", unsafe_allow_html=True)  
 st.write("")
 
-st.info("""Compare how regulatory features linked to transcriptional metaprograms vary across multiple glioblastoma samples.<br>
-  • TF: Examine transcription factors associated with each metaprogram to see which are consistently or differentially active across tumors.<br>
-  • Pathway: Explore pathway activation linked to specific metaprograms, uncovering shared or distinct signaling programs.<br>
-  • Drug: Assess predicted drug response signatures tied to different metaprograms, highlighting therapeutic vulnerabilities across the cohort.<br>
+st.info("""Compare how regulatory features linked to transcriptional metaprograms vary across multiple glioblastoma samples.  
+• TF: Examine transcription factors associated with each metaprogram to see which are consistently or differentially active across tumors.  
+• Pathway: Explore pathway activation linked to specific metaprograms, uncovering shared or distinct signaling programs.  
+• Drug: Assess predicted drug response signatures tied to different metaprograms, highlighting therapeutic vulnerabilities across the cohort.  
 Use interactive plots and selectors to identify regulatory patterns and drug sensitivities tied to key malignant and non-malignant programs.""")
-
 
 file = open('text_files/dotplot_tf_names.txt', 'r')
 list = file.read().splitlines()
