67cce9f9402e8868d29bb5b38e98475dac74a1cb
xim2016 <xj_ma@yahoo.com>
2025-07-16 22:35:47 -0400

    Update metaprogram_feature.py

--- views/metaprogram_feature.py
+++ views/metaprogram_feature.py
@@ -5,9 +5,6 @@ from views.utils import get_sample_metaprograms
 
 IMG_REPO = 'https://raw.githubusercontent.com/osmanbeyoglulab/gbm_data/main'
 
-IMG_REPO_2 = 'https://raw.githubusercontent.com/matthewlu2/gbm_small_data/main/dotplot_tf_activity'
-IMG_REPO_3 = 'https://raw.githubusercontent.com/matthewlu2/gbm_small_data/main/dotplot_pw_activity'
-IMG_REPO_4 = 'https://raw.githubusercontent.com/matthewlu2/gbm_small_data/main/dotplot_drug_score'
 
 st.markdown("<h2 style='text-align: center; color: black;'>Metaprogram Related Molecular Features</h1>", unsafe_allow_html=True)  
 st.write("")
@@ -36,18 +33,3 @@ st.markdown("<h3 style='text-align: center; color: black;'>Metaprogram by Drug2C
 st.image(f'{IMG_REPO}/across_metaprogram_top_drugs_per_sample/{option}.png')
 
 
-d_mp = get_sample_metaprograms("./data/sample_metaprograms.pkl")
-l_mp = d_mp[option]
-
-option_mp = st.selectbox(
-    'Metaprogram',
-    l_mp
-)
-st.markdown("<h3 style='text-align: center; color: black;'>Top TFs across samples</h1>", unsafe_allow_html=True)
-st.image(f'{IMG_REPO}/across_sample_top_transcriptions_per_metaprogram/{option_mp}.png')
-
-st.markdown("<h3 style='text-align: center; color: black;'>Top pathways across samples</h1>", unsafe_allow_html=True)
-st.image(f'{IMG_REPO}/across_sample_top_pathways_per_metaprogram/{option_mp}.png')
-
-st.markdown("<h3 style='text-align: center; color: black;'>Top drugs across samples</h1>", unsafe_allow_html=True)
-st.image(f'{IMG_REPO}/across_sample_top_drugs_per_metaprogram/{option_mp}.png')
