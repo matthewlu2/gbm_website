8fd0a9c36d3a931332cc0076e3b70ce0d825e09b
xim2016 <xj_ma@yahoo.com>
2025-07-16 21:18:51 -0400

    Update metaprogram_sample.py

--- views/metaprogram_sample.py
+++ views/metaprogram_sample.py
@@ -22,42 +22,6 @@ option = st.selectbox(
     key = persist("sample_id")
     ) 
 
-b2, c2, d2 = st.columns([.001, .10, .1])
-b, c, d = st.columns([.02, .12, .12])
-
-c2.markdown("<h3 style='text-align: center; color: black;'>HE Stain</h1>", unsafe_allow_html=True)
-
-d2.markdown("<h3 style='text-align: center; color: black;'>Metaprogram Proportion</h1>", unsafe_allow_html=True)
-# c.write("")
-
-c.image(f'{IMG_REPO}/he_stain/{option}.png')
-d.image(f'{IMG_REPO}/pie_metaprogram/{option}.png')
-
-
-
-# -- Single Metaprogram Images --
-
-# def url_is_alive(url):
-#     """
-#     Checks that a given URL is reachable.
-#     :param url: A URL
-#     :rtype: bool
-#     """
-#     request = urllib.request.Request(url)
-#     request.get_method = lambda: 'HEAD'
-#     try:
-#         urllib.request.urlopen(request)
-#         return True
-#     except urllib.request.HTTPError:
-#         return False
-    
-# metaprograms = ['AC','Chromatin.Reg','Inflammatory.Mac','MES.Ast','MES.Hyp','MES','Mac','NPC','Neuron','OPC','Oligo','Prolif.Metab','Reactive.Ast','Vasc']
-# l_mp = []
-# for mp in metaprograms:
-#     urlpath = f"https://github.com/matthewlu2/gbm_data/blob/main/metaprogram_tab/metaprogram_{mp}/{option}.png"
-#     if url_is_alive(urlpath):
-#         # l_mp.append(f'{IMG_REPO}/metaprogram_{mp}/{option}.png')
-#         l_mp.append(mp)
 
 d_mp = get_sample_metaprograms("./data/sample_metaprograms.pkl")
 l_mp = d_mp[option]
