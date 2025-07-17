73f7974e2285dfe1f95c6fb50cc5f113d1b29875
xim2016 <xj_ma@yahoo.com>
2025-07-17 19:58:19 -0400

    Update start.py

--- start.py
+++ start.py
@@ -210,7 +210,7 @@ pg = st.navigation(
     {
         "": [home_page, datasets_page],
         # "Analysis of Individual Samples": [metaprogram_page, metaprogram_feature_page, gene_page, s_tf_page, s_pathway_page , drug2cell_page],  # ligand_page,
-        # "Comparison Across Samples": [dotplot_page,  heatmap_gene_correlation_page], 
+        "Comparison Across Samples": [dotplot_page,  heatmap_gene_correlation_page], 
         "Resources": [citation_page, contact_page]
     }
 )
