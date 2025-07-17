5f30713d3121fd8bd355b14ab0c314350f2e3158
xim2016 <xj_ma@yahoo.com>
2025-07-17 14:17:06 -0400

    Update start.py

--- start.py
+++ start.py
@@ -133,25 +133,25 @@ citation_page = st.Page(
 
 # -- NAVIGATION --
 
-# pg = st.navigation(
-#     {
-#         "Overview": [home_page, datasets_page],
-#         "Analysis of Individual Samples": [metaprogram_page, metaprogram_feature_page, gene_page, s_tf_page, s_pathway_page , drug2cell_page],  # ligand_page,
-#         "Comparison Across Samples": [dotplot_page,  heatmap_gene_correlation_page], 
-#         "Others": [citation_page, contact_page]
-#     }
-# )
-
 pg = st.navigation(
     {
-        "Home": home_page,
-        "Dataset Explorer": datasets_page,
+        "Overview": [home_page, datasets_page],
         "Analysis of Individual Samples": [metaprogram_page, metaprogram_feature_page, gene_page, s_tf_page, s_pathway_page , drug2cell_page],  # ligand_page,
         "Comparison Across Samples": [dotplot_page,  heatmap_gene_correlation_page], 
-        "Citation": citation_page, 
-        "Contact Us": contact_page
+        "Others": [citation_page, contact_page]
     }
 )
+
+# pg = st.navigation(
+#     {
+#         "Home": home_page,
+#         "Dataset Explorer": datasets_page,
+#         "Analysis of Individual Samples": [metaprogram_page, metaprogram_feature_page, gene_page, s_tf_page, s_pathway_page , drug2cell_page],  # ligand_page,
+#         "Comparison Across Samples": [dotplot_page,  heatmap_gene_correlation_page], 
+#         "Citation": citation_page, 
+#         "Contact Us": contact_page
+#     }
+# )
 
 
 # -- SHARED ON ALL PAGES --
