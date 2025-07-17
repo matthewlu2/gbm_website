883cbef05c5b3c1d08667d56a7c313085f8807dd
xim2016 <xj_ma@yahoo.com>
2025-07-17 18:35:34 -0400

    Update start.py

--- start.py
+++ start.py
@@ -63,19 +63,19 @@ home_page = st.Page(
 datasets_page = st.Page(
     page = "views/dataset.py",
     title = "Dataset Explorer",
-    icon = ":material/keyboard_double_arrow_right:"  
+    icon = ":material/chevron_right:"  
 )
 
 metaprogram_page = st.Page(
     page = "views/metaprogram.py",
     title = "Metaprogram Maps",
-    icon = ":material/fiber_manual_record:"    
+    icon = ":material/chevron_right:"    
 )
 
 metaprogram_feature_page = st.Page(
     page = "views/metaprogram_feature.py",
     title = "Metaprogram-Associated Features",
-    icon = ":material/circle:"    
+    icon = ":material/chevron_right:"    
 )
 
 dotplot_page = st.Page(
@@ -136,10 +136,10 @@ citation_page = st.Page(
 
 pg = st.navigation(
     {
-        "Overview": [home_page, datasets_page],
+        "": [home_page, datasets_page],
         "Analysis of Individual Samples": [metaprogram_page, metaprogram_feature_page, gene_page, s_tf_page, s_pathway_page , drug2cell_page],  # ligand_page,
         "Comparison Across Samples": [dotplot_page,  heatmap_gene_correlation_page], 
-        "Others": [citation_page, contact_page]
+        "Support": [citation_page, contact_page]
     }
 )
 
