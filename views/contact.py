5d5b06b9705f011dda8748705d66398a75eb8d49
xim2016 <xj_ma@yahoo.com>
2025-07-18 18:26:55 -0400

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -9,9 +9,9 @@ st.markdown("<h3 style='text-align: center; color: black; font-size:25px;'>Unive
 st.markdown("""<div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5051%20Centre%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
 st.write("")
 
-a, b, c = st.columns([.0001, .7, .0001])
-b.markdown('##### If you have any questions or feedback, please feel free to contact us:')
-a2, b2, _ = st.columns([ .3, .3, .1])
+b, c = st.columns([ .7, .0001])
+st.markdown('##### If you have any questions or feedback, please feel free to contact us:')
+a2, b2, _ = st.columns([ .25, .25, .1])
 a2.markdown("""<span style="font-size:18px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
 a2.markdown("""<span style="font-size:18px;">Linan Zhang<br>Assistant Professor<br>✉️ liz133@pitt.edu</span>""", unsafe_allow_html=True)
 b2.markdown("""<span style="font-size:18px;">Matthew Lu<br>Undergraduate Researcher<br>✉️ mal554@pitt.edu</span>""", unsafe_allow_html=True)
