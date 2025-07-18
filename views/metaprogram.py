74dea9d72b6d9b08ebfb8a930f590dc94433ded9
xim2016 <xj_ma@yahoo.com>
2025-07-17 20:12:46 -0400

    Update metaprogram.py

--- views/metaprogram.py
+++ views/metaprogram.py
@@ -25,7 +25,7 @@ option = st.selectbox(
 b1, c1, d1 = st.columns([.001, .10, .1])
 b, c, d = st.columns([.02, .12, .12])
 
-c1.markdown("<h3 style='text-align: center; color: black;'>H&E Stain</h2>", unsafe_allow_html=True)
+c1.markdown("<h4 style='text-align: center; color: black;'>H&E Stain</h4>", unsafe_allow_html=True)
 
 #  with d1:
             
@@ -42,20 +42,20 @@ c1.markdown("<h3 style='text-align: center; color: black;'>H&E Stain</h2>", unsa
 c.image(f'{IMG_REPO}/he_stain/{option}.png')
 
 
-b2, c2, d2 = st.columns([.001, .10, .1])           
+b2, c2, d2 = st.columns([.00, .10, .1])           
 c2.markdown("<h3 style='text-align: center; color: black;'>Metaprogram Proportion</h2>", unsafe_allow_html=True)
 d2.markdown("<h3 style='text-align: center; color: black;'>Metaprogram</h2>", unsafe_allow_html=True)
 # c.write("")
 
 b3, c3, d3 = st.columns([.002, .12, .12])
-c2.image(f'{IMG_REPO}/pie_metaprogram/{option}.png')
-d2.write("")
-d2.write("")
-d2.write("")
-d2.write("")
-d2.write("")
-d2.write("")
-d2.image(f'{IMG_REPO}/metaprogram/{option}.png')
+c3.image(f'{IMG_REPO}/pie_metaprogram/{option}.png')
+d3.write("")
+d3.write("")
+d3.write("")
+d3.write("")
+d3.write("")
+d3.write("")
+d3.image(f'{IMG_REPO}/metaprogram/{option}.png')
 
 
 # d_mp = get_sample_metaprograms("./data/sample_metaprograms.pkl")
