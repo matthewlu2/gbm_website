a1b9a44870e8eb04d6c8805f3ff87629e386e82d
xim2016 <xj_ma@yahoo.com>
2025-07-17 20:28:32 -0400

    Update metaprogram.py

--- views/metaprogram.py
+++ views/metaprogram.py
@@ -27,7 +27,7 @@ b, c, d = st.columns([.002, .12, .12])
 
 c1.markdown("<h4 style='text-align: center; color: black;'>H&E Stain</h4>", unsafe_allow_html=True)
 
-
+st.write(option)
             
 d1.markdown( '<p style="font-family:sans-serif; color:#002e8c; font-size: 22px;  font-weight: bold">Sample {option}</p>',  unsafe_allow_html=True) 
 d1.write("")
