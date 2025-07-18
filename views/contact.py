ed8e0fa4260dbf19e8cc0351a71a8fda97549284
xim2016 <xj_ma@yahoo.com>
2025-07-18 18:13:28 -0400

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.markdown("<h2 style='text-align: center; color: black;'>Contact Us</h1>", unsafe_allow_html=True)  
 st.write("")
 
-a, b, c = st.columns([.3, .7, .3])
-b.markdown("""<div style="width: 100%"><iframe width="100%" height="700" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5051%20Centre%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
+a, b, c = st.columns([.1, .7, .1])
+b.markdown("""<div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5051%20Centre%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
 # a.image('https://wexfordscitech.com/wp-content/uploads/2021/03/Assembly-web-5.png')
 b.markdown("""<span style="font-size:25px;">The Department of Biomedical Informatics (DBMI) at the University of Pittsburgh School of Medicine</span>""", unsafe_allow_html=True)
 
