a4841a6b7be5694cf2002c7c80eda47b7e97ac77
xim2016 <xj_ma@yahoo.com>
2025-07-17 21:20:44 -0400

    Update metaprogram.py

--- views/metaprogram.py
+++ views/metaprogram.py
@@ -26,7 +26,7 @@ b1, c1, d1 = st.columns([.001, .08, .1])
 
 
 c1.markdown("<h4 style='text-align: center; color: black;'>H&E Stain</h4>", unsafe_allow_html=True)
-c1.markdown("<h4 style='text-align: center; color: black;'> </h4>", unsafe_allow_html=True)
+d1.markdown("<h4 style='text-align: center; color: black;'> temp</h4>", unsafe_allow_html=True)
 b, c, d = st.columns([.003, .12, .12])
 c.image(f'{IMG_REPO}/he_stain/{option}.png')
 
