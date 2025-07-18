1bdf44c0a85301207af33a8543fc917958b4ce00
xim2016 <xj_ma@yahoo.com>
2025-07-18 18:22:51 -0400

    Update contact.py

--- views/contact.py
+++ views/contact.py
@@ -7,14 +7,16 @@ st.markdown("<h3 style='text-align: center; color: black; font-size:25px;'>The D
 st.markdown("<h3 style='text-align: center; color: black; font-size:25px;'>University of Pittsburgh School of Medicine</h3>", unsafe_allow_html=True)
 
 st.markdown("""<div style="width: 100%"><iframe width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=5051%20Centre%20Avenue%20Pittsburgh,%20PA%2015213+(My%20Business%20Name)&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe></div>""", unsafe_allow_html=True)
+st.write("")
 
 a, b, c = st.columns([.1, .7, .1])
 # a.image('https://wexfordscitech.com/wp-content/uploads/2021/03/Assembly-web-5.png')
 # b.markdown("""<span style="font-size:25px;">The Department of Biomedical Informatics (DBMI) at the University of Pittsburgh School of Medicine</span>""", unsafe_allow_html=True)
 # b.markdown("<h3 style='text-align: center; color: black; font-size:25px;'>The Department of Biomedical Informatics (DBMI)<br> at the University of Pittsburgh School of Medicine</h3>", unsafe_allow_html=True)
 b.markdown('##### If you have any questions or feedback, please feel free to contact us:')
-b.markdown("""<span style="font-size:18px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
-b.markdown("""<span style="font-size:18px;">Linan Zhang<br>Assistant Professor<br>✉️ liz133@pitt.edu</span>""", unsafe_allow_html=True)
-b.markdown("""<span style="font-size:18px;">Matthew Lu<br>Undergraduate Researcher<br>✉️ mal554@pitt.edu</span>""", unsafe_allow_html=True)
-b.markdown("""<span style="font-size:18px;">Xiaojun Ma<br>Software Engineer<br>✉️ xim33@pitt.edu</span>""", unsafe_allow_html=True)
+_, a2, b2, _ = st.columns([.1, .3, .3, .1])
+a2.markdown("""<span style="font-size:18px;">Hatice Osmanbeyoglu<br>Principal Investigator<br>✉️ osmanbeyogluhu@pitt.edu</span>""", unsafe_allow_html=True)
+a2.markdown("""<span style="font-size:18px;">Linan Zhang<br>Assistant Professor<br>✉️ liz133@pitt.edu</span>""", unsafe_allow_html=True)
+b2.markdown("""<span style="font-size:18px;">Matthew Lu<br>Undergraduate Researcher<br>✉️ mal554@pitt.edu</span>""", unsafe_allow_html=True)
+b2.markdown("""<span style="font-size:18px;">Xiaojun Ma<br>Software Engineer<br>✉️ xim33@pitt.edu</span>""", unsafe_allow_html=True)
    
