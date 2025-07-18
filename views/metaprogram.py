8665b8e19b0a7edb1c3a3fe0ce899ce32fb5c683
xim2016 <xj_ma@yahoo.com>
2025-07-17 21:25:42 -0400

    Update metaprogram.py

--- views/metaprogram.py
+++ views/metaprogram.py
@@ -22,7 +22,7 @@ option = st.selectbox(
     key = persist("sample_id")
     ) 
 
-c1, d1 = st.columns([ 0.5, 0.6])
+c1,_, d1 = st.columns([ 0.5, 0.2, 0.6])
 
 
 c1.markdown("<h4 style='text-align: center; color: black;'>H&E Stain</h4>", unsafe_allow_html=True)
